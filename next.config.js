d38dbc647d7cdaf068f7654ff6b30d737b9f4d55
ARWeb <contact@arweb.dev>
2023-02-09 17:45:12 +0100

    clean

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 const nextConfig = {
-    basePath: '/polify',
+    /* basePath: '/polify', */
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
