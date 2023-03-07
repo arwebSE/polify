b821bb1f1de19cca96c9201bfff78044329dafa8
ARWeb <contact@arweb.dev>
2023-03-07 11:54:58 +0100

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.scss";
+import Script from "next/script";
 
 import type { AppProps } from "next/app";
 
@@ -8,5 +9,23 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const scriptUrl = "https://www.googletagmanager.com/gtag/js?id=" + process.env.ANALYTICS_ID;    
+    return (
+        <>
+            <Script strategy="afterInteractive" src={scriptUrl} />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.ANALYTICS_ID}');
+                    `,
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
