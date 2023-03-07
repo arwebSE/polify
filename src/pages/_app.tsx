ae3e69df2ec3e2160a9167fb6e2060812cec94cb
ARWeb <contact@arweb.dev>
2023-03-07 12:06:50 +0100

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.scss";
-import Script from "next/script";
 
 import type { AppProps } from "next/app";
-
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 export default function App({ Component, pageProps }: AppProps) {
-    const scriptUrl = "https://www.googletagmanager.com/gtag/js?id=" + process.env.ANALYTICS_ID;    
     return (
         <>
-            <Script strategy="afterInteractive" src={scriptUrl} />
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.ANALYTICS_ID}');
-                    `,
-                }}
-            />
+            <GoogleAnalytics trackPageViews />
             <Component {...pageProps} />
         </>
     );
